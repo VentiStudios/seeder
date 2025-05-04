a12d4eb7ec1461b986e7396d985dd1c9bafc707a
YiZhiMCQiu <pikachu20130124@163.com>
2025-05-04 08:18:17 +0800

    chore: 将原先 /finder 中的 Python 相关文件移动到 /finder/python

--- finder/run.py
+++
@@ -1,4 +0,0 @@
-from main_optimized import *
-
-if __name__ == "__main__":
-    print(find_special(array=[1, 1, 4, 5, 1, 4, 1, 9, 1, 9, 8, 1, 0], max_count=1, min_val=0, max_val=10, max_seed=2147483647, min_seed=0, threads=5))
